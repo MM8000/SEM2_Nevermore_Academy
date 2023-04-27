09f4d5a14ed0f5bc9ae1111d0dae61dc810bb4c3
MM8000 <76873821+MM8000@users.noreply.github.com>
2023-04-27 16:08:00 +0200

    THING goes crazy

---
+++ assets/js/function.js
@@ -0,0 +1,20 @@
+var imgmovement = document.getElementsByClassName("thing");
+var debounceTimer;
+
+for (var i = 0; i < imgmovement.length; i++) {
+  imgmovement[i].style.transition = "all 0.2s ease-in-out"; // add transition to all images
+}
+
+document.onmousemove = function(event) {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(function() {
+    var x = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
+
+    for (var i = 0; i < imgmovement.length; i++) {
+      imgmovement[i].style.left = x;
+      imgmovement[i].style.top = y;
+      imgmovement[i].style.transform = "translate(-" + x + ",-" + y + ")";
+    }
+  }, 10); // debounce time in milliseconds
+};
